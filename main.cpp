8df8e1f1b0ffad84f808a8f17b833d0f286b3314
shahnoor <shahnoor3pl@gmail.com>
2018-07-29 18:23:29 +0600

    commit 01

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+size_t factorial(size_t n){
+    if (n == 1) return 1;
+    else return n * factorial(n-1);
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    cout << factorial(10) << endl;
+
+    return 0;
+}
